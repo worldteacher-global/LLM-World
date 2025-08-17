031737006b930ac799fc7c577aa29ddd6bc4ab31
worldteacher-global <george.kofi.marfo@gmail.com>
2025-08-17 14:10:13 +0000

    LLM-World/app.py: chat like output

--- src/app.py
+++ src/app.py
@@ -65,9 +65,15 @@ if __name__=='__main__':
         #     st.markdown(prompt)        
         st.session_state.messages.append({"role": "human","content":prompt})
         
+        word = " "
         with st.chat_message("ai"):
+            
             response = chat_gpt_41(prompt)
-            st.markdown(response)
+            
+            for chunk in response:
+                word += chunk
+
+            st.markdown(response_generator(word))
         
         st.session_state.messages.append({"role":"ai", "content":chat_gpt_41(prompt)})
 
