18f52563b2071a31370548efced28085350432ed
worldteacher-global <george.kofi.marfo@gmail.com>
2025-08-20 20:36:01 -0400

    LLM-World/oss-train.py: trainer script

--- src/oss-train.py
+++ src/oss-train.py
@@ -6,18 +6,18 @@ import os
 os.environ["TRANSFORMERS_NO_TRACKIO"] = "1"  # disables internal tracking
 os.environ["TRL_GRADIO_ENABLED"] = "0"      # disables Gradio UI
 os.environ["WANDB_MODE"] = "offline"        # optional: disables WandB logging
-# -----------------------------
-
-# Now safe to import
-from trl import SFTTrainer
-from transformers import AutoModelForCausalLM, AutoTokenizer, Mxfp4Config
 
 # -----------------------------
-# Load your model and tokenizer
+import torch
+from trl import SFTTrainer, SFTConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, Mxfp4Config, TrainingArguments 
+from peft import LoraConfig, get_peft_model
+from datasets import load_dataset
+
+# -----------------------------
+
 model_name = "openai/gpt-oss-20b"
 tokenizer_name = model_name
-
-import torch
 
 quantization_config = Mxfp4Config(dequantize=True)
 
@@ -31,7 +31,7 @@ model_kwargs = dict(
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, **model_kwargs)
 
-from peft import LoraConfig, get_peft_model
+# -----------------------------
 
 peft_config = LoraConfig(
     r=8,                   # enable more trainable parameters
@@ -50,14 +50,9 @@ peft_config = LoraConfig(
 peft_model = get_peft_model(model,peft_config)
 # -----------------------------
 
-# -----------------------------
-# Define your training arguments
-
-from transformers import TrainingArguments
-
 filepath = '/home/sagemaker-user/user-default-efs/CLONED_REPOS/LLM-World/Files'
 
-training_args = TrainingArguments(
+training_args = TrainingArguments(    # SFTConfig
     output_dir=os.path.join(filepath,"gpt-oss-20b-multilingual-reasoner"),
     save_strategy="steps",
     save_steps=50,
@@ -68,7 +63,7 @@ training_args = TrainingArguments(
     logging_steps=1,
     per_device_train_batch_size=4,
     gradient_accumulation_steps=4, # accumulate gradients over multiple batches before weight update 
-    max_length=2048,
+    # max_length=2048,
     warmup_ratio=0.03,
     lr_scheduler_type="cosine_with_min_lr",
     lr_scheduler_kwargs={"min_lr_rate":0.1},
@@ -76,24 +71,16 @@ training_args = TrainingArguments(
 )
 # -----------------------------
 
-# -----------------------------
-# Define your dataset
-# Example: replace with your real dataset
-from datasets import load_dataset
-
 dataset = load_dataset("HuggingFaceH4/Multilingual-Thinking", split="train")
 # -----------------------------
 
-# -----------------------------
-# Initialize the trainer
 trainer = SFTTrainer(
     model=peft_model,
     args=training_args,
-    train_dataset=dataset,
-    tokenizer=tokenizer,
-    # processing_class if you have custom processing
+    train_dataset=dataset#.shuffle(buffer_size=1000),
+    processing_class=tokenizer   
 )
+# -----------------------------
 
-# Start training
 trainer.train()
 # -----------------------------
