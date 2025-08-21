e6199647b46d8571055c82af605ea88598150fa6
worldteacher-global <george.kofi.marfo@gmail.com>
2025-08-21 18:41:52 +0000

    LLM-World/agent_app.py: Updated title to include emojis

--- src/agent_app.py
+++ src/agent_app.py
@@ -140,7 +140,7 @@ def multiAgent(user_query: str, file_path: str | None = None) -> Tuple[str, str 
 if __name__=='__main__':
     st.set_page_config(layout="wide")
 
-    st.title(":pill: StatLine Agent :telephone_receiver:")
+    st.title(":telephone_receiver: StatLine :male-scientist:Agent:female-scientist:")
     if 'messages' not in st.session_state: st.session_state.messages = []
 
     for message in st.session_state.messages:
