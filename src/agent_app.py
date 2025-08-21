111b1faea214f5b2c0bee71dabd0d9b0c4056c1d
worldteacher-global <george.kofi.marfo@gmail.com>
2025-08-21 16:53:38 +0000

    LLM-World/agent_app.py: testing if file exists

--- src/agent_app.py
+++ src/agent_app.py
@@ -155,19 +155,19 @@ if __name__=='__main__':
     # if user_input := st.chat_input("Hello! Please submit a question or request:", accept_file=True, file_type=[".csv"]):
     user_input = st.chat_input("Hello! Please submit a question or request:", accept_file=True, file_type=[".csv"])        
     if user_input:
-        # st.write("user_input object:", user_input)
+        st.write("user_input object:", user_input)
         # st.write("user_input.file:", user_input.files[0].name)
         # st.write("user_input.text:", user_input.text)
         user_text = user_input.text if hasattr(user_input, "text") else str(user_input)
         # st.write(user_text)
-        file_path = None
-        if hasattr(user_input, "files") and user_input.files[0].name is not None:
-            uploaded_file = user_input.files[0]
-            file_path = os.path.join(UPLOAD_DIR, uploaded_file.name)            
-            with open(file_path, "wb") as f:
-                f.write(uploaded_file.file.read())
-                           
-            st.write(f"this is the path:{file_path}")
+        # file_path = None
+        # if hasattr(user_input, "files") and user_input.files[0].name is not None:
+        #     uploaded_file = user_input.files[0]
+        #     file_path = os.path.join(UPLOAD_DIR, uploaded_file.name)            
+        #     with open(file_path, "wb") as f:
+        #         f.write(uploaded_file.file.read())
+
+        #     st.write(f"this is the path:{file_path}")
         #     st.success(f"File saved to {file_path}")
 
         # st.session_state.messages.append({"role": "user", "content": user_text})       
